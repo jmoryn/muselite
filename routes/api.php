d37e9014bba5f8047f9136a95b4922a1a0d07b43
Jesse Moryn <macuser@Macs-MacBook-Pro.local>
2021-06-07 22:01:07 -0500

    add laravel breeze and passport

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/songs', function(){
-    return 'heres a song';
-});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
